0cbb293b7ad6fb978bf7533d426534a6985b0c6b
Anthony Mistretta <ar93mistretta@gmail.com>
2025-02-04 13:02:25 -0700

    Fix: Proper XCTest case for Xcode Cloud

---
+++ VoiceJournalTests/VoiceJournalTests.swift
@@ -0,0 +1,16 @@
+//
+//  VoiceJournalTests.swift
+//  VoiceJournalTests
+//
+//  Created by Anthony Mistretta on 2/4/25.
+//
+
+import Testing
+
+struct VoiceJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
